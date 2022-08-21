12a15d4c2b7cfb27f97f42d435d5315fcdfb3144
NasihNazeem <nasihnazeem@gmail.com>
2022-08-21 12:17:17 -0600

    Navbar created

---
+++ src/components/App.jsx
@@ -0,0 +1,22 @@
+
+export default function App() {
+
+  return (
+    <header>
+      <h1>Nasih Nazeem</h1>
+      <nav>
+        <ul>
+          <li><a href="Home.jsx">Home</a></li>
+          <li><a href="About.jsx">About</a></li>
+          <li><a href="Contact.jsx">Contact</a></li>
+          <li><a href="Portfolio.jsx">Portfolio</a></li>
+          <li><a href="Resume.jsx">Resume</a></li>
+          <li><a href="Blog.jsx">Blog</a></li>
+          <li><a href="Testimonials.jsx">Testimonials</a></li>
+          <li><a href="Hobbies.jsx">Hobbies</a></li>
+        </ul>
+
+      </nav>
+    </header>
+  );
+}
