155a48f42a35191351a0e4b997b29eca2a65b882
NasihNazeem <nasihnazeem@gmail.com>
2022-08-22 17:45:11 -0600

    only 4 navigators now

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,14 +6,10 @@ export default function App() {
       <h1>Nasih Nazeem</h1>
       <nav>
         <ul>
-          <li><a href="Home.jsx">Home</a></li>
           <li><a href="About.jsx">About</a></li>
           <li><a href="Contact.jsx">Contact</a></li>
           <li><a href="Portfolio.jsx">Portfolio</a></li>
-          <li><a href="Resume.jsx">Resume</a></li>
-          <li><a href="Blog.jsx">Blog</a></li>
-          <li><a href="Testimonials.jsx">Testimonials</a></li>
-          <li><a href="Hobbies.jsx">Hobbies</a></li>
+          <li><a href="Skills.jsx">Skills</a></li>
         </ul>
 
       </nav>
