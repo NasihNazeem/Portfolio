f4d8582124959578132b7bc1efe00005e7fd8d09
NasihNazeem <nasihnazeem@gmail.com>
2022-08-23 17:06:55 -0600

    Working on Navbar

--- src/components/misc/Navbar.jsx
+++ src/components/misc/Navbar.jsx
@@ -1,14 +1,28 @@
 import './navbar.scss';
+import PersonIcon from '@mui/icons-material/Person';
+import EmailIcon from '@mui/icons-material/Email';
 
 export default function Navbar(){
     return(
-        <div className='navbar'>
+        <div className='navbar active'>
             <div className="wrapper">
                 <div className="left">
                     <a href="#home" className='logo'>nazeem.</a>
+                    <div className='itemContainer'>
+                        <PersonIcon className="icon"/>
+                        <span>+1 403 700 7883</span>
+                    </div>
+                    <div className='itemContainer'>
+                        <EmailIcon className="icon"/>
+                        <span>nasihnazeem@gmail.com</span>
+                    </div>
                 </div>
                 <div className="right">
-                
+                    <div className="hamburger">
+                        <span className='line1'></span>
+                        <span className='line2'></span>
+                        <span className='line3'></span>
+                    </div>
                 </div>  
             </div>
         </div>
