93df9a917cad6f085ca923fa011ceaa52fd7be91
NasihNazeem <nasihnazeem@gmail.com>
2022-08-22 19:45:17 -0600

    Added Home Page, scroll is smoother, added name to navbar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import Navbar from "./misc/Navbar";
 import Contact from "./pages/contact/Contact";
+import Home from "./pages/home/Home";
 import About from "./pages/about/About";
 import Portfolio from "./pages/portfolio/Portfolio"; 
 import Skills from "./pages/skills/Skills"; 
@@ -15,6 +16,7 @@ export default function App() {
         <Navbar/>
       
       <div className="sections">
+        <Home />
         <About />
         <Skills />
         <Portfolio />
--- src/components/misc/Navbar.jsx
+++ src/components/misc/Navbar.jsx
@@ -3,7 +3,14 @@ import './navbar.scss';
 export default function Navbar(){
     return(
         <div className='navbar'>
-
+            <div className="wrapper">
+                <div className="left">
+                    <a href="#home" className='logo'>nazeem.</a>
+                </div>
+                <div className="right">
+                
+                </div>  
+            </div>
         </div>
     );
 }
