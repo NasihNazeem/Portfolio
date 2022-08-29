290dd2808af8570c347854aa28acd1cd5883930e
NasihNazeem <nasihnazeem@gmail.com>
2022-08-28 20:42:12 -0600

    added jpg

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -3,7 +3,12 @@ import "./home.scss";
 export default function Home() {
   return (
     <div className="home" id="home">
-
+      <div className="left">
+        <div className="imgContainer">
+          <img src="/assets/man.jpg" alt="profile-picture" />
+        </div>
+      </div>
+      <div className="right"></div>
     </div>
   )
 }
