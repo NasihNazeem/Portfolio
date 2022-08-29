bfb16848ba672522667c3970cb614c08a7190604
NasihNazeem <nasihnazeem@gmail.com>
2022-08-28 23:13:17 -0600

    Portfolio coming soon integrated

--- src/components/pages/portfolio/Portfolio.jsx
+++ src/components/pages/portfolio/Portfolio.jsx
@@ -1,9 +1,24 @@
+
+import { init } from 'ityped';
+import { useEffect, useRef } from "react";
 import './portfolio.scss';
 
-export default function Portfolio(){
+export default function Portfolio() {
+    const textRef = useRef();
+
+
+    useEffect(() => {
+        init(textRef.current, {
+            showCursor: false,
+            backDelay: 1500,
+            backSpeed: 40,
+            strings: ["soon...", "s00n...", "S00N...", "...soon"],
+        })
+    })
     return (
         <div className='portfolio' id='portfolio'>
-
+            <h1>Portfolio</h1>
+            <h2>Coming <span ref={textRef}></span></h2>
         </div>
 
     );
